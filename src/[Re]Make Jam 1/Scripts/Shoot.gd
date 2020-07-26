de27282a559cbf35e43a90987af516ed80a1ef0d
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-07-26 09:46:58 -0300

    Cuando un disparo impacta contra un enemigo, ambos desaparecen

--- src/[Re]Make Jam 1/Scripts/Shoot.gd
+++ src/[Re]Make Jam 1/Scripts/Shoot.gd
@@ -3,9 +3,14 @@ extends Area2D
 var speed = -200
 
 func _ready():
-	pass # Replace with function body.
+	connect("body_entered", self, "_on_Shoot_body_entered")
 
 func _process(delta):
 	var velocity = Vector2(0, 1)
 	velocity = velocity.normalized() * speed
 	position += velocity * delta
+
+func _on_Shoot_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.queue_free()
+		queue_free()
